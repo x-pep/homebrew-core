e85dc34ae5e0e216a9b192e2be2196306f971e57
Claudio Luck <claudio.luck@pep.foundation>
2018-02-27 17:22:56 +0100

    init

---
+++ Formula/pep-core-libetpan.rb
@@ -0,0 +1,27 @@
+class PepCoreLibetpan < Formula
+  desc ""
+  homepage ""
+  version "0.1"
+  url "https://software.pep.foundation/enigmail/source/libetpan-0.1.tar.gz"
+  sha256 "6a7d325821c928816c607a955953989be064378351c1bd280006e60dc883b298"
+
+  def install
+    system "./autogen.sh", "--prefix=#{prefix}"
+    system "make"
+    system "/usr/bin/install_name_tool",
+               "-id", "@rpath/libetpan.17.dylib",
+               "./src/.libs/libetpan.17.dylib"
+    system "/usr/bin/install_name_tool",
+               "-add_rpath", "@executable_path/../lib",
+               "./src/.libs/libetpan.17.dylib"
+    system "make", "install"
+    # bin.install "dmidecode"
+    # man8.install "man/dmidecode.8"
+  end
+
+  # test do
+  #  system "#{bin}/dmidecode"
+  # end
+
+end
+
---
+++ Formula/pep-core-yml2.rb
@@ -0,0 +1,9 @@
+class PepAdapterEnigmail < Formula
+  desc ""
+  homepage ""
+  url "https://software.pep.foundation/enigmail/source/test-0.1.tar.gz"
+  sha256 "c34c73ba7aca7c04de5ea26611e071dbbb4893821452dc6ce472ccba8bdf961e"
+
+  depends_on "pkg-config" => :run
+end
+
