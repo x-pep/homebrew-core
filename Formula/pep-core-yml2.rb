209c9f7f79d7ef700c068b869c5694e85cde7012
Claudio Luck <claudio.luck@pep.foundation>
2018-02-27 17:34:28 +0100

    more

--- Formula/pep-core-yml2.rb
+++ Formula/pep-core-yml2.rb
@@ -1,7 +1,7 @@
-class PepAdapterEnigmail < Formula
+class PepCoreYml2 < Formula
   desc ""
   homepage ""
-  url "https://software.pep.foundation/enigmail/source/test-0.1.tar.gz"
+  url "https://software.pep.foundation/enigmail/source/yml2-0.1.tar.gz"
   sha256 "c34c73ba7aca7c04de5ea26611e071dbbb4893821452dc6ce472ccba8bdf961e"
 
   depends_on "pkg-config" => :run
